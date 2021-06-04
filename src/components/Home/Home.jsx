8e620ae950cceba97a448eb10d9f507c3627e58c
Paulína Palíková <paulipalik@gmail.com>
2021-06-04 18:40:22 +0200

    home1

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import './style.scss';
 
 const Home = () => {
-  return <p>Jsem domovská stránka</p>;
+  return (
+    <div className="container">
+      <h1>Create a CV that will land you a job</h1>
+      <p>
+        Looking for a job can be challenging, but we are here to empower you to
+        make the best first impression with your CV.
+      </p>
+      <button className="create-cv">Create a CV</button>
+      <img src={require('./img/13038.jpg')} alt="computer-cv"></img>
+    </div>
+  );
 };
 
 export default Home;
+{
+  /* <img src={require('./logo.jpeg')} /> */
+}
