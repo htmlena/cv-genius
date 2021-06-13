73b8366a57e639d6465dc812aca00ef09559dc04
Paulína Palíková <paulipalik@gmail.com>
2021-06-13 20:52:25 +0200

    keywords-fix

--- src/components/Keywords/Keywords.jsx
+++ src/components/Keywords/Keywords.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../Generator/style.css';
 import InfoTooltip from '../InfoTooltip/InfoTooltip';
 import '../Generator/style.css';
