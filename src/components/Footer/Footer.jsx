5fc699efb84900fe0a795a13c95efb2fe58f1683
Lenka Shynkarova <o.shynkarova@gmail.com>
2021-06-13 11:07:36 +0200

    small css fixes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,6 @@ const Footer = () => {
         <div className="developed-links-container">
           ©{' '}
           <a
-            rel="noreferrer"
             className="developed-link"
             href="https://github.com/paulipa1"
             target="_blank"
@@ -18,7 +17,6 @@ const Footer = () => {
           </a>{' '}
           &{' '}
           <a
-            rel="noreferrer"
             className="developed-link"
             href="https://htmlena.dev/"
             target="_blank"
@@ -30,7 +28,6 @@ const Footer = () => {
       </div>
       <div className="resources-info">
         <a
-          rel="noreferrer"
           className="resource-link"
           href="https://www.freepik.com/vectors/hand"
           target="_blank"
@@ -39,7 +36,6 @@ const Footer = () => {
           Hand vector created by pch.vector - www.freepik.com
         </a>
         <a
-          rel="noreferrer"
           className="resource-link"
           href="https://zety.com/blog/resume-statistics"
           target="_blank"
